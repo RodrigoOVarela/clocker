c72f916dc1b4f3908257e9b56b129d065b2bd17c
RODRIGO Varela <rodrigooliveiravarela@gmail.com>
2021-08-30 23:42:57 -0300

    fix: ajuste para uso do webpack5

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   webpack(config) {
-      config.module.rules.push({
-          test: /\.svg$/,
-          issuer: {
-              test: /\.(js|ts)x?$/,
-          },
-          use: ['@svgr/webpack'],
-      });
+    config.module.rules.push({
+      test: /.svg$/,
+      issuer: {
+        // test: /.(js|ts)x?$/,
+        // for webpack 5 use
+        and: [/.(js|ts)x?$/]
+      },
 
-      return config;
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
